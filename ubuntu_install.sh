a36991f16ba1518ebc75b209ad4ce420a20bfe70
erka <erdenebold10@gmail.com>
2022-02-28 01:33:36 +0800

    error edit

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -15,7 +15,11 @@ sudo apt-get install –f
 sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
 sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
 sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
-sudo apt-get install postgresql-13
+sudo apt -y install vim bash-completion wget
+sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt-get update
+sudo apt-get install postgresql-13 postgresql-client-13
 sudo su postgres
 createuser odoo -s;
 psql template1
